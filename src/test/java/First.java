f3125445bfcc7f5178358705d17f6a784b18a203
Rai <ayurai@deloitte.com>
2022-03-25 15:32:11 +0530

    Fourth assignment

---
+++ src/test/java/First.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class First{
+    static WebDriver driver;
+    static String username = "", password = "", first_name = "", last_name = "", pin_code = "600600";
+
+    @Test(priority = 1)
+    public static void excel_reader() throws IOException {
+        String line = "";
+        String splitBy = ",";
+        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\ayurai\\Registration.csv"));
+        while ((line = br.readLine()) != null) {
+            String[] login = line.split(splitBy);
+            username = login[0];
+            password = login[1];
+            break;
+        }
+        BufferedReader br2 = new BufferedReader(new FileReader("C:\\Users\\ayurai\\Booking_Details.csv"));
+        while ((line = br2.readLine()) != null) {
+            String[] cust = line.split(splitBy);
+            first_name = cust[0];
+            last_name = cust[1];
+            pin_code = cust[2];
+            break;
+        }
+    }
+}
